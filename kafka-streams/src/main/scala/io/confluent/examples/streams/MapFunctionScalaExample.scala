42d3b833acb71f06d28256fb64e4aa568304ce1e
Connie Chen <cchen@twilio.com>
2017-07-17 15:12:05 -0700

    Example that creates multiple KStreams

--- kafka-streams/src/main/scala/io/confluent/examples/streams/MapFunctionScalaExample.scala
+++ kafka-streams/src/main/scala/io/confluent/examples/streams/MapFunctionScalaExample.scala
@@ -19,7 +19,8 @@ import java.util.Properties
 
 import org.apache.kafka.common.serialization._
 import org.apache.kafka.streams._
-import org.apache.kafka.streams.kstream.{KStream, KStreamBuilder}
+import org.apache.kafka.streams.kstream.{GlobalKTable, KStream, KStreamBuilder}
+import org.apache.kafka.streams.state.QueryableStoreTypes
 
 /**
   * Demonstrates how to perform simple, state-less transformations via map functions.
@@ -92,63 +93,47 @@ import org.apache.kafka.streams.kstream.{KStream, KStreamBuilder}
   */
 object MapFunctionScalaExample {
 
+  val BOOTSTRAP_SERVERS = "localhost:9092"
+  val APPLICATION_ID = "map-function-scala-example-testing"
+  val CONFIG_TOPIC = "StreamConfig3"
+
   def main(args: Array[String]) {
-    val bootstrapServers = if (args.length > 0) args(0) else "localhost:9092"
-    val builder = new KStreamBuilder
+
+    val configBuilder = new KStreamBuilder
 
     val streamingConfig = {
       val settings = new Properties
-      settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "map-function-scala-example")
-      settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+      settings.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID)
+      settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS)
       // Specify default (de)serializers for record keys and for record values.
-      settings.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray.getClass.getName)
+      settings.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
       settings.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
+      settings.put(StreamsConfig.STATE_CLEANUP_DELAY_MS_CONFIG, "600000")
       settings
     }
 
-    val stringSerde: Serde[String] = Serdes.String()
+    val config: GlobalKTable[String, String] = configBuilder.globalTable(CONFIG_TOPIC, "test-store")
 
-    // Read the input Kafka topic into a KStream instance.
-    val textLines: KStream[Array[Byte], String] = builder.stream("TextLinesTopic")
+    val configStream = new KafkaStreams(configBuilder, streamingConfig)
+    configStream.start()
 
-    // Variant 1: using `mapValues`
-    val uppercasedWithMapValues: KStream[Array[Byte], String] = textLines.mapValues(_.toUpperCase())
+    val view = configStream.store("test-store", QueryableStoreTypes.keyValueStore[String, String])
+    println("Looking at config")
+    val iterator = view.all()
+    println("\n\n\n\n\n iterator created")
 
-    // Write (i.e. persist) the results to a new Kafka topic called "UppercasedTextLinesTopic".
-    //
-    // In this case we can rely on the default serializers for keys and values because their data
-    // types did not change, i.e. we only need to provide the name of the output topic.
-    uppercasedWithMapValues.to("UppercasedTextLinesTopic")
+    // Create a buffer to store all the stream creation results
+    //var streamsCreated = new mutable.ListBuffer[Try[Unit]]()
 
-    // We are using implicit conversions to convert Scala's `Tuple2` into Kafka Streams' `KeyValue`.
-    // This allows us to write streams transformations as, for example:
-    //
-    //    map((key, value) => (key, value.toUpperCase())
-    //
-    // instead of the more verbose
-    //
-    //    map((key, value) => new KeyValue(key, value.toUpperCase())
-    //
-    import KeyValueImplicits._
-
-    // Variant 2: using `map`, modify value only (equivalent to variant 1)
-    val uppercasedWithMap: KStream[Array[Byte], String] = textLines.map((key, value) => (key, value.toUpperCase()))
-
-    // Variant 3: using `map`, modify both key and value
-    //
-    // Note: Whether, in general, you should follow this artificial example and store the original
-    //       value in the key field is debatable and depends on your use case.  If in doubt, don't
-    //       do it.
-    val originalAndUppercased: KStream[String, String] = textLines.map((key, value) => (value, value.toUpperCase()))
-
-    // Write the results to a new Kafka topic "OriginalAndUppercasedTopic".
-    //
-    // In this case we must explicitly set the correct serializers because the default serializers
-    // (cf. streaming configuration) do not match the type of this particular KStream instance.
-    originalAndUppercased.to(stringSerde, stringSerde, "OriginalAndUppercasedTopic")
-
-    val stream: KafkaStreams = new KafkaStreams(builder, streamingConfig)
-    stream.start()
+    while(iterator.hasNext()) {
+      val nextValue = iterator.next()
+      val topic = nextValue.key.toString
+      val dest = nextValue.value.toString
+      println(s"HAS NEXT $topic $dest \n\n\n\n\n\n")
+      val transformer = new StreamTransformer(topic, dest, APPLICATION_ID)
+      transformer.run()
+    }
+    configStream.close()
   }
 
 }
---
+++ kafka-streams/src/main/scala/io/confluent/examples/streams/StreamTransformer.scala
@@ -0,0 +1,8 @@
+package io.confluent.examples.streams
+
+/**
+  * Created by cchen on 7/17/17.
+  */
+class StreamTransformer {
+
+}
